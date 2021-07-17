624b71a1849c62f8bf842db2c0f8d0840d292d42
Ilya Bakhar <ilya_bakhar@bk.ru>
2021-07-17 13:01:46 +0300

    Add count_person_on_video

--- count_person_on_video.py
+++ count_person_on_video.py
@@ -1,9 +1,12 @@
-import os
 import sys
+import os
 import face_recognition
 import cv2 as cv 
 import csv
 import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
 
 
 def get_frame(path_to_video, frame_number):
@@ -32,12 +35,17 @@ def person_on_frame():
 
     known_enc = [] #enc = encoding
     images = os.listdir('out_frames')
-    
+    list_per_second = [] 
+    person_per_second = [] 
+
     for(i, image) in enumerate(images):
         print(f'Обробатываемый кадр:[{i + 1}/{len(images)}]')
         face_img = face_recognition.load_image_file(f'out_frames/{image}')
         face_codes = face_recognition.face_encodings(face_img)
-        print(f'На кадре обнаруженно:[{len(face_codes)}] лица')        
+        print(f'На кадре обнаруженно:[{len(face_codes)}] лица')
+        list_per_second.append(i)
+        person_per_second.append(len(face_codes))
+
         if len(face_codes) == 0:
                 print('На этом кадре НЕТ лиц')
                 continue
@@ -48,11 +56,12 @@ def person_on_frame():
                 for item in range(0, len(known_enc)):
                     result = face_recognition.compare_faces(face_codes[code], known_enc[item])
                     if True in result:
-                        print('Этот человек посчитан на предидущем кадре')
+                        print('Этот человек посчитан на предЫдущем кадре')
                     else:
                         known_enc[0].append(face_codes[code])   
-    print(f'ОБНАРУЖЕННО: {len(known_enc[0])} людей')
-    return known_enc[0] # Получаем результат в виде списка
+    print(f'ОБНАРУЖЕННО: {len(known_enc[0])} человека')
+    return known_enc[0], list_per_second, person_per_second # Получаем результат в виде списка
+
 
 
 def list_to_dict(enc_to_transform):
@@ -73,14 +82,25 @@ def save_result(transformed_enc, path_to_csv):
             writer.writerow(row)
 
 
+def show_result(person_per_second, list_per_second, enc_to_transform):
+    """"Рисуем график"""
+    fig, ax = plt.subplots(figsize=(5, 3))
+    ax.stackplot(list_per_second, person_per_second, labels =['Количество человек'])
+    ax.set_title(f'Всего на видео {len(enc_to_transform)} человек(а)')
+    ax.legend(loc='upper left')
+    ax.set_ylabel('Люди')
+    ax.set_xlabel('Время')
+    ax.set_xlim(xmax= list_per_second[-1], xmin = list_per_second[0])
+    fig.tight_layout()
+    plt.savefig('Test_chart.png')
+
+
 def main():
-    get_frame('video\example_2.mp4', 100)
-    enc_to_transform = person_on_frame()
+    get_frame('video\example_5.mp4', 33)
+    enc_to_transform, list_per_second, person_per_second = person_on_frame()
     transformed_enc = list_to_dict(enc_to_transform)
     save_result(transformed_enc, 'result.csv')
-
+    show_result(person_per_second, list_per_second, enc_to_transform)
 
 if __name__ == '__main__':
     main()
-
-#
